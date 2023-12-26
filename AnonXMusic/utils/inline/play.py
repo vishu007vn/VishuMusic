db3913b5353477ef4df3cfe62109e29f2e10666b
Dhiman / <154400066+DHIMXN@users.noreply.github.com>
2023-12-26 08:50:23 +0530

    Update play.py

--- AnonXMusic/utils/inline/play.py
+++ AnonXMusic/utils/inline/play.py
@@ -74,13 +74,19 @@ def stream_markup_timer(_, chat_id, played, dur):
 def stream_markup(_, chat_id):
     buttons = [
         [
-            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
-            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
-            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
-            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
-            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
+           InlineKeyboardButton(text="Sᴜᴘᴘᴏʀᴛ",url="https://t.me/Punjabi_Hindi_Chat"),
         ],
-        [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
+        [
+           InlineKeyboardButton(text="Uᴘᴅᴀᴛᴇs",url=f"https://t.me/Dhiman_Network",
+            ),
+           InlineKeyboardButton(text="Oᴡɴᴇʀ",url=f"https://t.me/i_dxlvir",
+            ),
+        ],
+        [
+            InlineKeyboardButton(
+                text="✯ ᴄʟᴏsᴇ ✯", callback_data="close"
+            )
+        ],
     ]
     return buttons
 

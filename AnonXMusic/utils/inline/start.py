013e304e72914aa1e227138495bda5ca6c6f5a4c
Vishulover680 <vishal255g@gmail.com>
2025-10-23 18:46:25 +0530

    Update support button URL in start.py

--- AnonXMusic/utils/inline/start.py
+++ AnonXMusic/utils/inline/start.py
@@ -31,7 +31,7 @@ def private_panel(_):
         ],
         [
             InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
-            InlineKeyboardButton(text=_["S_B_7"], url="https://t.me/Abt_Mei"),
+            InlineKeyboardButton(text=_["S_B_7"], url="https://t.me/pallavisarkaar"),
         ],
     ]
     return buttons
